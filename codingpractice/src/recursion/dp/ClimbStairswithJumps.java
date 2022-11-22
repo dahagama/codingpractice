883ac5e87085a5b1d59d257889625c5bc636e4aa
dahagama <dahagama@gmail.com>
2022-11-22 10:01:24 -0700

    yy

--- codingpractice/src/recursion/dp/ClimbStairswithJumps.java
+++ codingpractice/src/recursion/dp/ClimbStairswithJumps.java
@@ -17,11 +17,11 @@ public class ClimbStairswithJumps {
 		
 		int[] dp=new int[n+1];
 		dp[n]=1;
-		for (int i = n - 1; i >= 0; i--) {
-		      for (int reach = i + 1; reach <= Math.min(n, i + arr[i]); reach++) {
-		        dp[i] += dp[reach];
-		      }
-		    }
+		for(int i=n-1;i>=0;i--) {
+			for(int j=1;j<=arr[i] && i+j<dp.length;j++) {
+				dp[i]+=dp[i+j];
+			}
+		}
 		System.out.println(dp[0]);
 		scn.close();
 	}
