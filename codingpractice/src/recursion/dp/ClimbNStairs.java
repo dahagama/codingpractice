18dd9a450083d94ffcd6edb46139b69a9391f098
dahagama <dahagama@gmail.com>
2022-11-22 08:57:52 -0700

    123

---
+++ codingpractice/src/recursion/dp/ClimbNStairs.java
@@ -0,0 +1,30 @@
+package recursion.dp;
+
+import java.util.Scanner;
+
+public class ClimbNStairs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n= scn.nextInt();
+		
+		int[] arr = new int[n];
+		for(int i=0;i<arr.length;i++) {
+			arr[i]= scn.nextInt();
+		}
+		
+		int[] dp = new int[n+1];
+		dp[n]=1;
+		
+		for(int i=n-1;i>=0;i--) {
+			for(int j=1;j<=arr[i] && i+j<dp.length;j++) {
+				dp[i] += dp[i+j];
+			}
+		}
+		
+		System.out.println(dp[0]);
+		scn.close();
+	}
+
+}
