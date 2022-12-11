5ef2435c7c6088cce20f9bf98ab4d629ae27879b
dahag <dahagama@gmail.com>
2022-12-11 12:51:36 -0700

    BFS and DFS init

---
+++ codingpractice/src/datastructures/BFSGraphTraversal.java
@@ -0,0 +1,11 @@
+package datastructures;
+
+//Queue
+public class BFSGraphTraversal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ codingpractice/src/datastructures/DFSGraphTraversal.java
@@ -0,0 +1,11 @@
+package datastructures;
+
+//Stack
+public class DFSGraphTraversal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
