9aaf40119f415824177c98b13a9e1e3856cc1f1f
dahag <dahagama@gmail.com>
2022-12-19 10:12:38 -0700

    bubble sort

--- codingpractice/src/recursion/dp/Climb123StairsDP.java
+++ codingpractice/src/recursion/dp/Climb123StairsDP.java
@@ -32,12 +32,12 @@ public class Climb123StairsDP {
 
 		    dp[0] = 1;
 		    for (int i = 1; i <= n; i++) {
-		      dp[i] = dp[i - 1];
-		      if (i >= 2)
-		        dp[i] += dp[i - 2];
-		      if (i >= 3)
-		        dp[i] += dp[i - 3];
-		    }
+		        dp[i] = dp[i - 1];
+		        if (i >= 2)
+		          dp[i] += dp[i - 2];
+		        if (i >= 3)
+		          dp[i] += dp[i - 3];
+		      }
 
 		    return dp[n];
 		  }
--- codingpractice/src/sorting/BubbleSort.java
+++ codingpractice/src/sorting/BubbleSort.java
@@ -1,5 +1,32 @@
 package sorting;
 
+import java.util.Arrays;
+
 public class BubbleSort {
+	
+	static void bubbleSortAsc(int a[])
+	{
+		int i,j,t;
+		for(i=0;i<a.length-1;i++)
+		{
+			//bubble sort
+			for(j=0;j<a.length-i-1;j++)
+			{
+				if(a[j] > a[j+1])
+				{
+					t = a[j];
+					a[j] = a[j+1];
+					a[j+1] = t;
+				}
+			}
+		}
+	}
+	
+	public static void main(String args[]) {
+		int a[] = {1,9,3,8,7,5,2,4};
+		System.out.println("before sorting=====>"+Arrays.toString(a));
+		BubbleSort.bubbleSortAsc(a);
+		System.out.println("after sorting asc==>"+Arrays.toString(a));
+	}
 
 }
